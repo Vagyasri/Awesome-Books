79158ccd5f3b2320ab8a27ca5ef5d67a50c94da0
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-07-05 16:41:58 +0200

    Add a new book

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const listOfBooks = [
      {title: 'book2 title', author:'author 2'}
 ];
 
+ function AddBook(title, author, collection){
+    collection.push({title: title, author: author})
+}
+
+AddBook('book3', 'author-3', listOfBooks);
+console.log(listOfBooks);
+
+
