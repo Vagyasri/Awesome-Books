d6e5a97e039e85b00477199c1e4459c781ebf1d0
Lucky <vagyasri2011@gmail.com>
2021-07-05 17:29:55 +0200

    Add remove book function

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const listOfBooks = [
+let listOfBooks = [
      {title: 'book1 title', author:'author 1'},
      {title: 'book2 title', author:'author 2'}
 ];
 
- function AddBook(title, author, collection){
+ function addBook(title, author, collection){
     collection.push({title: title, author: author})
 }
 
-AddBook('book3', 'author-3', listOfBooks);
-console.log(listOfBooks);
+// AddBook('book3', 'author-3', listOfBooks);
+// console.log(listOfBooks);
 
+function removeBook(remTitle, collection){
+ listOfBooks = collection.filter(book => book.title != remTitle);
+}
+
+// RemoveBook('book2 title', listOfBooks);
+// console.log(listOfBooks);
+
+function display(collection){
+     
+}
 
