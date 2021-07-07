7fc8892af57d9559b88c6935f0b7fabd8f41d7ec
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-07-07 15:28:13 +0200

    Add Book class

--- draft.js
+++ draft.js
@@ -1,38 +1,34 @@
-// // let listOfBooks = JSON.parse(localStorage.getItem('books')) || [];
-// // let listOfBooks = [];
+// let listOfBooks = JSON.parse(localStorage.getItem('books')) || [];
+// let listOfBooks = [];
 
-// class Book {
-//     static listofBooks = JSON.parse(globalThis.localStorage.getItem('books')) || [];
+class Book {
+    static listOfBooks = JSON.parse(globalThis.localStorage.getItem('books')) || [];
 
-//     static #numOfBooks = 0;
+    static #numOfBooks = 0;
 
-//     constructor({ title, author }) {
-//       this.title = title;
-//       this.author = author;
-//       Book.#numOfBooks += 1;
-//       this.serialNumber = Book.#numOfBooks;
-//       Book.listOfBooks.push(this);
-//     }
-// }
-// class Methods {
-//   constructor() {}
+    constructor({ title, author }) {
+      this.title = title;
+      this.author = author;
+      Book.#numOfBooks += 1;
+      this.serialNumber = Book.#numOfBooks;
+      Book.listOfBooks.push(this);
+    }
+}
 
-//   static add({ title, author }) {
-//     //
-//     new Book({ title, author });
-//     Book.listofBooks;
-//   }
-// }
 
-// // console.log(Book.listofBooks);
 
-// // // let method1 = new Methods();
-// // // Methods.add();
+
+// Methods.add({title: 'CLASSmy-book', author:'class'});
+//console.log(Book.listofBooks)
+
+
+
+
 
 // let book1 = new Book({ author:'author', title:'first-book'});
 
-// // let book2 = new Book({'2nd-book', 'author'});
-// // let book3 = new Book({'3rd-book', 'author'});
+// let book2 = new Book({'2nd-book', 'author'});
+// let book3 = new Book({'3rd-book', 'author'});
 
 // console.log(book1)
-// // // console.log(book1.author)
+// // console.log(book1.author)
