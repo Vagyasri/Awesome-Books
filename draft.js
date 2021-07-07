4246161183ad4d12494e5ec44b7f2d609a861fc8
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-07-07 14:36:11 +0200

    factoring-classes

---
+++ draft.js
@@ -0,0 +1,40 @@
+// // let listOfBooks = JSON.parse(localStorage.getItem('books')) || [];
+// // let listOfBooks = [];
+
+// class Book {
+//     static listofBooks = JSON.parse(globalThis.localStorage.getItem('books')) || [];
+
+//     static #numOfBooks = 0;
+
+//     constructor({ title, author }) {
+//       this.title = title;
+//       this.author = author;
+//       Book.#numOfBooks += 1;
+//       this.serialNumber = Book.#numOfBooks;
+//       Book.listOfBooks.push(this);
+//     }
+// }
+// class Methods {
+//   constructor() {}
+
+//   static add({ title, author }) {
+//     // 
+//     new Book({ title, author });
+//     Book.listofBooks;
+//   }
+// }
+
+// // console.log(Book.listofBooks);
+
+// // // let method1 = new Methods();
+// // // Methods.add();
+
+
+
+// let book1 = new Book({ author:'author', title:'first-book'});
+
+// // let book2 = new Book({'2nd-book', 'author'});
+// // let book3 = new Book({'3rd-book', 'author'});
+
+// console.log(book1)
+// // // console.log(book1.author)
